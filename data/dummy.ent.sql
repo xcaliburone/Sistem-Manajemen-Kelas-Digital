0cc2dbc4a4ee24bdec3e452cac261fce7586a4f1
adhimrahman <adhimrahaman@gmail.com>
2024-10-08 17:07:33 +0800

    add:

---
+++ data/dummy.ent.sql
@@ -0,0 +1,34 @@
+INSERT INTO `guru` (`nip`, `nama`,          `password`) VALUES
+    ('200310312025011001', 'adhim rahman',  'kenapamengapakarena'),
+    ('200310312025011002', 'dean gery',     'kocak123'),
+    ('200310312025011003', 'raihan miftah', 'imang123');
+ 
+INSERT INTO `mata_pelajaran` (`id`, `nama`) VALUES
+    ('MP001',   'matematika'),
+    ('MP002',   'fisika'),
+    ('MP003',   'biologi');
+
+INSERT INTO `kelas` (`id`, `nama`) VALUES
+    ('KL001',   'X MIPA 1'),
+    ('KL002',   'X MIPA 2'),
+    ('KL003',   'X MIPA 3');
+
+INSERT INTO `absen` (`id`, `nama`, `tanggal`) VALUES
+    ('AB001',   ''),
+    ();
+
+INSERT INTO `siswa` (`nisn`, `nama`, `password`) VALUES
+    (),
+    ();
+
+INSERT INTO `penilaian` (`id`, `nilai`, `tanggal`, 'keterangan') VALUES
+    (),
+    ();
+
+INSERT INTO `tugas` (`id`, `judul`, `deskripsi`, `status`, `deadline`, `jenis`, `tanggal`) VALUES
+    (),
+    ();
+
+INSERT INTO `materi` (`id`, `tanggal`, `judul`, `konten`) VALUES
+    (),
+    ();
---
+++ data/dummy.rel.sql
@@ -0,0 +1,3 @@
+INSERT INTO `guru` (`nip`, `nama`, `password`) VALUES
+    ('200310312025011001', 'adhim rahman', 'kenapamengapakarena'),
+    ('200310312025011003', 'raihan miftah', 'imang123');
