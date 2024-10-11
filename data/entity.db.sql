bffa1e65f685f519f244e2ec74217da9967d0c27
adhimrahman <adhimrahaman@gmail.com>
2024-10-11 23:50:38 +0800

    fix: id primary key

--- data/entity.db.sql
+++ data/entity.db.sql
@@ -10,25 +10,25 @@ CREATE TABLE IF NOT EXISTS `guru` (
 );
 
 CREATE TABLE IF NOT EXISTS `mata_pelajaran` (
-    `id` VARCHAR(36) NOT NULL DEFAULT (SELECT CONCAT('MP', UUID())),
+    `id` VARCHAR(36) NOT NULL,
     `nama` VARCHAR(32) NOT NULL,
     PRIMARY KEY (`id`),
     INDEX `idx_mapel_nama` (`nama`)
 );
 
 CREATE TABLE IF NOT EXISTS `kelas` (
-    `id` VARCHAR(36) NOT NULL DEFAULT (SELECT CONCAT('KL', UUID())),
+    `id` VARCHAR(36) NOT NULL,
     `nama` VARCHAR(32) NOT NULL,
     PRIMARY KEY (`id`),
     INDEX `idx_kls_nama` (`nama`)
 );
 
 CREATE TABLE IF NOT EXISTS `absen` (
-    `id` VARCHAR(36) NOT NULL DEFAULT (SELECT CONCAT('AB', UUID())),
-    `nama` VARCHAR(32) NOT NULL,
+    `id` VARCHAR(36) NOT NULL,
+    `status` ENUM('hadir', 'alfa', 'sakit', 'izin', 'none') NOT NULL DEFAULT 'none',
     `tanggal` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
-    INDEX `idx_absen_nama` (`nama`)
+    INDEX `idx_abs_status` (`status`)
 );
 
 CREATE TABLE IF NOT EXISTS `siswa` (
@@ -40,15 +40,16 @@ CREATE TABLE IF NOT EXISTS `siswa` (
 );
 
 CREATE TABLE IF NOT EXISTS `penilaian` (
-    `id` VARCHAR(36) NOT NULL DEFAULT (SELECT CONCAT('NL', UUID())),
+    `id` VARCHAR(36) NOT NULL,
     `nilai` DECIMAL(5, 2) NOT NULL,
     `tanggal` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `keterangan` TEXT DEFAULT NULL,
     PRIMARY KEY (`id`),
+    INDEX `idx_pnln_nilai` (`nilai`)
 );
 
 CREATE TABLE IF NOT EXISTS `tugas` (
-    `id` VARCHAR(36) NOT NULL DEFAULT (SELECT CONCAT('TG', UUID())),
+    `id` VARCHAR(36) NOT NULL,
     `judul` VARCHAR(32) NOT NULL,
     `deskripsi` TEXT DEFAULT NULL,
     `status` ENUM('undone', 'done') NOT NULL DEFAULT 'undone',
@@ -56,14 +57,14 @@ CREATE TABLE IF NOT EXISTS `tugas` (
     `jenis` ENUM('tugas', 'kuis', 'ujian') NOT NULL DEFAULT 'tugas',
     `tanggal` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
-    INDEX `idx_kls_name` (`judul`)
+    INDEX `idx_tugas_judul` (`judul`)
 );
 
 CREATE TABLE IF NOT EXISTS `materi` (
-    `id` VARCHAR(36) NOT NULL DEFAULT (SELECT CONCAT('MT', UUID())),
+    `id` VARCHAR(36) NOT NULL,
     `tanggal` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `judul` VARCHAR(32) NOT NULL,
     `konten` TEXT DEFAULT NULL,
     PRIMARY KEY (`id`),
-    INDEX `idx_kls_name` (`judul`)
+    INDEX `idx_materi_judul` (`judul`)
 );
